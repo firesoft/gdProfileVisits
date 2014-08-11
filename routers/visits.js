aee651010caab3b3367ee8194f118bd949f186e7
Piotr Bxx <firesoft1983@gmail.com>
2014-08-11 22:27:23 +0200

    visits remove route

--- routers/visits.js
+++ routers/visits.js
@@ -3,8 +3,10 @@
 var router = require('express').Router();
 var visitsGetController = require('../controllers/visits_get.js');
 var visitsVisitController = require('../controllers/visits_visit.js');
+var visitsRemoveController = require('../controllers/visits_remove.js');
 
 router.get('/visits/:playerId', visitsGetController.validate, visitsGetController.get);
 router.post('/visits/:playerId', visitsVisitController.validate, visitsVisitController.visit);
+router.delete('/visits/:playerId', visitsRemoveController.validate, visitsRemoveController.remove);
 
 module.exports = router;
